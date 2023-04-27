d096be26f380db64b6cf8b87d1e4b5e9713e76b2
Sylvain <90060355+Sylvain-Mani@users.noreply.github.com>
2023-04-27 14:01:06 +0200

    Mise en place de la structure de l'appli (classes, vue main page)

---
+++ BTS-Chopesgames-maui/Models/Marque.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTS_Chopesgames_maui.Models
+{
+    internal class Marque
+    {
+    }
+}
